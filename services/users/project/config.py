effe84eed077b721daef8660898ba0e332a152d4
darkniter <Poranot@yandex.ru>
2019-08-22 12:48:25 +0300

    change config example

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,6 +5,7 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SECRET_KEY = 'my_precious'  # new
 
 
 class DevelopmentConfig(BaseConfig):
