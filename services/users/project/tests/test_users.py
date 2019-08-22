6ebd919974a99bf1e4ee061290e8ee6d5bfb4170
darkniter <Poranot@yandex.ru>
2019-08-22 15:40:13 +0300

    add test create user

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -18,6 +18,21 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'michael',
+                    'email': 'michael@mherman.org'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('michael@mherman.org was added!', data['message'])
+            self.assertIn('success', data['status'])
 
 if __name__ == '__main__':
     unittest.main()
