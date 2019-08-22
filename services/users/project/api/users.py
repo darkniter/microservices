8ec5b7a56cfff2d9336d0f2e49a453827d48d840
darkniter <Poranot@yandex.ru>
2019-08-22 15:51:41 +0300

    add creation users in users.py

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,6 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
 
+from project import db
+from project.api.models import User
 
 users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
@@ -15,3 +17,19 @@ class UsersPing(Resource):
 
 
 api.add_resource(UsersPing, '/users/ping')
+
+
+class UsersList(Resource):
+    def post(self):
+        post_data = request.get_json()
+        username = post_data.get('username')
+        email = post_data.get('email')
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+        response_object = {
+            'status': 'success',
+            'message': f'{email} was added!'
+        }
+        return response_object, 201
+
+api.add_resource(UsersList, '/users')
