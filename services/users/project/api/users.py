0d1a19dd041d9f7615a8774d4675146edbc3d1ca
darkniter <Poranot@yandex.ru>
2019-08-22 15:06:56 +0300

    aaaaaa bl*&t'

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# services/users/project/api/users.py
-
-
-=======
->>>>>>> 13c148538f8155c5b77a12ca930b98675a080cc6
 from flask import Blueprint
 from flask_restful import Resource, Api
 
