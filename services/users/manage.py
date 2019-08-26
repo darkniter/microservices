ecbde158c422fbef92db99914b023e5b48c8f413
darkniter <Poranot@yandex.ru>
2019-08-26 11:20:20 +0300

    add seed_db command

--- services/users/manage.py
+++ services/users/manage.py
@@ -30,5 +30,12 @@ def test():
     sys.exit(result)
 
 
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
 if __name__ == '__main__':
     cli()
