5032f9002f1ba6897085eb07fd4063fd452ba02c
darkniter <Poranot@yandex.ru>
2019-08-28 18:32:11 +0300

    fix UserList in index

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import UsersList from './components/UsersList';
 
 // new
 class App extends Component {
