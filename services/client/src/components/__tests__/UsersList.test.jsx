53ebcd4c1c563972b0ce073e7af04ba1088693c7
darkniter <Poranot@yandex.ru>
2019-08-28 18:47:28 +0300

    add snapshot test

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import renderer from 'react-test-renderer';
 import UsersList from '../UsersList';
+
 
 const users = [
   {
@@ -18,9 +19,15 @@ const users = [
   }
 ];
 
+
 test('UsersList renders properly', () => {
   const wrapper = shallow(<UsersList users={users}/>);
   const element = wrapper.find('h4');
   expect(element.length).toBe(2);
   expect(element.get(0).props.children).toBe('michael');
+});
+
+test('UsersList renders a snapshot properly', () => {
+  const tree = renderer.create(<UsersList users={users}/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
