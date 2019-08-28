2ce62b9805ac2994a482d8b9f1dddf27b64dc374
darkniter <Poranot@yandex.ru>
2019-08-28 17:05:06 +0300

    add index.js

---
+++ services/client/src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';  // new
+import ReactDOM from 'react-dom';
+
+
+// new
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
