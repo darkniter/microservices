fffa8866d32490a8c61290f1a7187757cecbcf96
darkniter <Poranot@yandex.ru>
2019-08-28 18:01:56 +0300

    add support cross origin requests

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,12 +5,14 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension  # new
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS  # new
 
 
 # instantiate the extensions
 db = SQLAlchemy()
-toolbar = DebugToolbarExtension()  # new
+toolbar = DebugToolbarExtension()
+cors = CORS()  # new
 
 
 def create_app(script_info=None):
@@ -24,7 +26,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
-    toolbar.init_app(app)  # new
+    toolbar.init_app(app)
+    cors.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
