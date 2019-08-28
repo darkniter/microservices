b7fe4beffb3275ed6b00601b5726f74f1c425dbd
darkniter <Poranot@yandex.ru>
2019-08-28 17:38:03 +0300

    modify index.js

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';  // new
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-one-third">
-            <br/>
-            <h1 className="title is-1 is-1">All Users</h1>
-            <hr/><br/>
+
+// new
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 };
 
 ReactDOM.render(
