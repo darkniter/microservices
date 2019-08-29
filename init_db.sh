20b0580d0ce764b893299d41bea48918fb7c5ae4
darkniter <Poranot@yandex.ru>
2019-08-29 15:01:40 +0300

    add client in compose file

--- init_db.sh
+++ init_db.sh
@@ -5,5 +5,3 @@ docker-compose up -d --build
 docker-compose exec users python manage.py test
 docker-compose exec users python manage.py recreate_db
 docker-compose exec users python manage.py seed_db
-
-
--- services/client/init_react.sh
+++ services/client/init_react.sh
@@ -4,8 +4,8 @@ sudo npm install create-react-app@2.1.8 --global
 
 create-react-app .
 
-npm install axios@0.18.0 --save
+sudo npm install axios@0.18.0 --save
 
-npm install --save-dev enzyme@3.9.0 enzyme-adapter-react-16@1.11.2
+sudo npm install --save-dev enzyme@3.9.0 enzyme-adapter-react-16@1.11.2
 
-npm install react-scripts@2.1.8 --global.
+sudo npm install react-scripts@2.1.8 --global.
