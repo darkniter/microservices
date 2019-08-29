5a2041c161d457363370921d12f212e93cf3ab09
darkniter <Poranot@yandex.ru>
2019-08-29 12:48:11 +0300

    add react script

---
+++ init_react.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm install create-react-app@2.1.8 --global
+
+create-react-app .
+
+npm install axios@0.18.0 --save
+
+npm install --save-dev enzyme@3.9.0 enzyme-adapter-react-16@1.11.2
+
+npm install react-scripts@2.1.8 --global.
