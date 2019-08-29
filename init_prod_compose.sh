fedc7316a81f921d96fff3abbf0dfedbfcdab0db
darkniter <Poranot@yandex.ru>
2019-08-29 17:23:14 +0300

    tryed change for local build

--- init_prod_compose.sh
+++ init_prod_compose.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker-compose -f docker-compose-prod.yml up -d --build
 
-docker-compose exec users python manage.py test
+docker-compose exec users sh -c "while ! nc -z users-db 5432; do sleep 1; done; python manage.py test"
 docker-compose exec users python manage.py recreate_db
 docker-compose exec users python manage.py seed_db
